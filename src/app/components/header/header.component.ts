310b3bdb3f867e1680c2da6478ad33ae2f888bfa
hunteman <pivovarov.d@mail.ru>
2023-03-11 18:41:43 +0700

    add styles and header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+  pageTitle: string; 
+
+  @Input () set title(val: string) {
+    this.pageTitle = val;
+  }
+}
